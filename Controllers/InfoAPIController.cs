0d9a000986cc061f09eaf59d807d89b7299e7647
Faisal Khan <Faisal.Khan@investisdigital.com>
2023-03-14 11:03:37 +0530

    Added Validations and Alerts

---
+++ Controllers/InfoAPIController.cs
@@ -0,0 +1,42 @@
+﻿using DemoMVC.Data;
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoAPIController : ControllerBase
+    {
+        private readonly DemoMVCDbContext demoMVCDbContext;
+        public InfoAPIController(DemoMVCDbContext demoMVCDbContext)
+        {
+            this.demoMVCDbContext = demoMVCDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetInfo()
+        {
+           var user = await demoMVCDbContext.InfoModel.ToListAsync();
+           return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostInfo(AddInfoViewModel request)
+        {
+            var user = new InfoModel()
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                StoredFrom = request.StoredFrom
+            };
+            await demoMVCDbContext.InfoModel.AddAsync(user);
+            await demoMVCDbContext.SaveChangesAsync();
+            return Ok(user);
+
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddAPI()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddAPI(AddInfoViewModel request)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7007/api/InfoAPI");
+
+                var postTask = client.PostAsJsonAsync<AddInfoViewModel>("InfoAPI", request);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("AddAPI");
+                }
+
+                ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+                return View(request);
+            }
+        }
+    }
+}
